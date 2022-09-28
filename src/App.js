12116dbfe41ae08b904031a1a965e1d6c2f4aee2
Sherry <hhlsdkm@gmail.com>
2022-09-28 16:46:39 +0900

    Switch & exact

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
 
@@ -11,15 +11,17 @@ const App = () => {
     <div>
       <MainHeader />
       <main>
-        <Route path={"/welcome"}>
-          <Welcome />
-        </Route>
-        <Route path={"/products"}>
-          <Products />
-        </Route>
-        <Route path={"/product-detail/:productId"}>
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path={"/welcome"}>
+            <Welcome />
+          </Route>
+          <Route path={"/products"} exact>
+            <Products />
+            <Route path={"/products/:productId"}>
+              <ProductDetail />
+            </Route>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
