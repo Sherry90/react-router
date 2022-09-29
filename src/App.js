f9cb0a2d32ee151a8c357221f7f4863c2068f0ee
Sherry <hhlsdkm@gmail.com>
2022-09-29 10:35:06 +0900

    practice setup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import { Route, Switch, Redirect } from "react-router-dom";
-
-import MainHeader from "./components/MainHeader";
-
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import ProductDetail from "./pages/ProductDetail";
-
-const App = () => {
+function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-            <Route path="/products/:productId">
-              <ProductDetail />
-            </Route>
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
-};
+}
 
 export default App;
