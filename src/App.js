3fa3f5ee7c2bb37610096f8261511de1ecfa240c
Sherry <hhlsdkm@gmail.com>
2022-09-29 12:27:04 +0900

    exact route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
+import { Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
+const App = () => {
   return (
     <div>
-
+      <Switch>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
