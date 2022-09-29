53ecb8b617e93ca6021c3300a19d14c857728e46
Sherry <hhlsdkm@gmail.com>
2022-09-29 14:45:02 +0900

    Redirection & useParams

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,14 @@
+import { useParams } from "react-router-dom";
+import { Fragment } from "react";
+
 const QuoteDetail = () => {
-  return <h1>Quotes Detail Page</h1>;
+  const params = useParams();
+  return (
+    <Fragment>
+      <h1>Quotes Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
