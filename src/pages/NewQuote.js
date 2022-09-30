e7477d0c431bdb3921212ed4927b02cb2d7fbc28
Sherry <hhlsdkm@gmail.com>
2022-09-30 12:15:05 +0900

    history push

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQouteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQouteHandler} />;
